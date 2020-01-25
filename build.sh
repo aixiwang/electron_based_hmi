ec3e2b9fe8ccdf9f11cf2c6f45b7b4990db2ff26
Aixi Wang <aixi.wang@hotmail.com>
2020-01-25 20:22:57 +0800

    first checkin

---
+++ build.sh
@@ -0,0 +1,1 @@
+electron-packager . --overwrite --platform=linux --arch=x64 --icon=assets/icons/png/1024x1024.png --prune=true --out=release-builds
---
+++ install/start.sh
@@ -0,0 +1,3 @@
+cd /home/test/loader
+./main https://profaceamerica.com/en-US/content/web-hmi
+
